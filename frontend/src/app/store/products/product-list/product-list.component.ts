28152ede20b87bae10981ab3780328c3e4f8be9f
Andrey <andrey201223@ukr.net>
2023-10-14 00:38:21 +0300

    added dynamic route to product-list component

--- frontend/src/app/store/products/product-list/product-list.component.ts
+++ frontend/src/app/store/products/product-list/product-list.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-product-list',
@@ -8,6 +10,13 @@ import { CommonModule } from '@angular/common';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+
+  constructor (private readonly route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(p => console.log(p['category'])
+    );
+  }
 
 }
