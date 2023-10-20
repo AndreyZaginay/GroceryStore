2fa962c2f6800bc8b4b7a8c4fe0da4367908738b
Vladyslav Vyslobitskyi <vyslobitskyi.vlad@gmail.com>
2023-10-20 17:42:48 +0300

    added firestore service

--- frontend/src/app/services/firestore.service.ts
+++ frontend/src/app/services/firestore.service.ts
@@ -1,21 +1,56 @@
 import { inject } from '@angular/core';
-import { collection, Firestore, getDocs, query, where } from '@angular/fire/firestore';
-import {concatAll, from, map, Observable, toArray} from "rxjs";
+import { addDoc, collection, deleteDoc, doc, Firestore, getDoc, getDocs, query, updateDoc, where } from '@angular/fire/firestore';
+import { concatAll, from, map, Observable, toArray } from "rxjs";
+
+export interface FirestoreDocument {
+  id: string;
+}
+
+export type AddFirestoreDocument<T extends FirestoreDocument> = Omit<T, 'id'>;
+export type UpdateFirestoreDocument<T extends FirestoreDocument> = Partial<AddFirestoreDocument<T>>;
 
 export type QueryFilterConstraint = ReturnType<typeof where>;
 
-export abstract class AbstractFirebaseService<T> {
+export abstract class BaseFirestoreService<T extends FirestoreDocument> {
   private readonly firestore = inject(Firestore);
 
   protected getDocs(path: string, constraints?: QueryFilterConstraint): Observable<T[]>;
   protected getDocs(path: string, constraints: QueryFilterConstraint): Observable<T[]> {
     const colRef = collection(this.firestore, path);
-    let docsQuery = query(colRef, constraints);
-    return from(getDocs(docsQuery)).pipe(
+    let getDocs$ = from(getDocs(colRef));
+    if (constraints) {
+      const docsQuery = query(colRef, constraints);
+      getDocs$ = from(getDocs(docsQuery));
+    }
+    return getDocs$.pipe(
       map((querySnapshot) => querySnapshot.docs),
       concatAll(),
-      map((doc) => ({id: doc.id, ...doc.data()}) as T),
+      map((docSnap) => ({id: docSnap.id, ...docSnap.data()}) as T),
       toArray()
     );
+  }
+
+  protected getDoc(path: string) {
+    const docRef = doc(this.firestore, path);
+    return from(getDoc(docRef)).pipe(
+      map((docSnap) => ({id: docSnap.id, ...docSnap.data()}) as T)
+    )
+  }
+
+  protected addDoc(path: string, data: AddFirestoreDocument<T>) {
+    const colRef = collection(this.firestore, path);
+    return from(addDoc(colRef, data)).pipe(
+      map((doc) => doc.id)
+    );
+  }
+
+  protected updateDoc(path: string, data: UpdateFirestoreDocument<T>) {
+    const docRef = doc(this.firestore, path);
+    return from(updateDoc(docRef, data));
+  }
+
+  protected deleteDoc(path: string) {
+    const docRef = doc(this.firestore, path);
+    return from(deleteDoc(docRef));
   }
 }
