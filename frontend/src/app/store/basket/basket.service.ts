4a979563c0b5ee144795bdac4f363a40c581f539
Andrey <andrey201223@ukr.net>
2023-11-05 03:13:00 +0200

    added and optimized methods in basket service

--- frontend/src/app/store/basket/basket.service.ts
+++ frontend/src/app/store/basket/basket.service.ts
@@ -16,19 +16,27 @@ export class BasketService {
   constructor() { }
 
   addPurchase(product: Product): void {
+    product.measurement === 'kg' ? this.addPurchaseByMeasurement(0.1, product) : this.addPurchaseByMeasurement(1, product);
+  }
+
+  addPurchaseByMeasurement(value: number, product: Product) {
     const target = this.purchases.find((purchase) => purchase.product.name === product.name);
     if (!target) {
-      this.purchases.push({ count: 1, product });
+      this.purchases.push({ count: value, product });
       this.emitPurchases(this.purchases);
       return;
     }
-    target.count++;
+    target.count = +(target.count + value).toFixed(1);
     this.emitPurchases(this.purchases);
   }
 
   decreasePurchase(product: Product): void {
+    product.measurement === 'kg' ? this.decreasePurchaseByMeasurement(0.1, product) : this.decreasePurchaseByMeasurement(1, product);
+  }
+
+  decreasePurchaseByMeasurement(value: number, product: Product) {
     const target = this.purchases.find((purchase) => purchase.product.name === product.name)!;
-    target.count--;
+    target.count = +(target.count - value).toFixed(1);
     if (target.count === 0) {
       this.removePurchase(product);
       this.emitPurchases(this.purchases);
